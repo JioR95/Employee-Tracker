474adeadf498e392173982db63d877ca74658292
JioR95 <jiovani.rivera@rocketmail.com>
2022-10-19 16:48:16 -0400

    getroles function added

--- utils/role.js
+++ utils/role.js
@@ -1,3 +1,17 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const { getDepartmentsArray } = require('./department');
+
+async function getRoles() {
+    const mysql = require('mysql2/promise');
+    const conn = await mysql.createConnection({ 
+        host: 'localhost',
+        user: 'root',
+        password: 'Jae0327!',
+        database: 'employee_tracker'
+    });
+    const [rows, fields] = await conn.execute(`SELECT roles.id, roles.title, departments.name AS department, roles.salary
+    FROM roles LEFT JOIN departments ON roles.departments_id = departments.id`);
+    await conn.end();
+    return rows;
+}
